4a2be80ddbf88afdb0aa7a79eba88b4e17df75f9
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2020-06-21 00:46:39 -0400

    Primer collider y rigidbody
    Ya puse un ballon a elevarse

---
+++ Alien versus Ballons/Assets/Scripts/Ballon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballon : MonoBehaviour
+{
+
+    [SerializeField] Vector3 Fuerza;
+    [SerializeField] Sprite[] ballonSprites;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Fuerza = new Vector3(UnityEngine.Random.Range(-100,100),UnityEngine.Random.Range(150,250));
+
+        rb.AddForce(Fuerza);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
