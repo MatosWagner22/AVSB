5dbf9a2fd4e529984312ecce5da54c752409caa2
elvinsomon <elvinsomon2001@gmail.com>
2020-06-22 12:14:54 -0400

    Se agrego el player
    Se agregó el player y sus propiedades de movimiento.

---
+++ Alien versus Ballons/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Awake2");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Awake3");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.position.x, Input.GetAxis("Vertical") * speed, transform.position.z);
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Awake4");
+
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Awake5");
+
+    }
+
+}
