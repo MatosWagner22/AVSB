d13d337e0b994f5e6f11cc1c06c5227c23fa3b91
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2020-06-23 18:19:14 -0400

    Pausa Agregada
    Se agrego el canvas y la funcion de pausa y se reajusto el tamaño de los ballons

---
+++ Alien versus Ballons/Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    
+    bool activo;
+    Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            Pausar();
+            activo = !activo;
+            canvas.enabled = activo;
+        }
+    }
+    
+    public void Pausar(){
+        if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+
+}
