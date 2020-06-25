dfa6d19f8ede66b3156ccb15838bc5400789da4d
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2020-06-25 17:34:58 -0400

    Laser y LimitesPlayer v.1
    Ya dispara y si pasa de los limiter del alien muere

---
+++ Alien versus Ballons/Assets/Scripts/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] UnityEngine.Vector3 bulletForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Shooter();
+        }
+    }
+
+    void Shooter()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddRelativeForce(bulletForce);
+    }
+
+}
