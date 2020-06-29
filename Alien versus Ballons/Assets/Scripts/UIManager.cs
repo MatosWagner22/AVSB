73f1a95873814f5e4f4199c1386c394b72511ffb
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2020-06-28 21:35:28 -0400

    Titulo y Final Agregado
    Ya tiene un menu y cuando llega a cierta cantidad de puntos tiene un final

--- Alien versus Ballons/Assets/Scripts/UIManager.cs
+++ Alien versus Ballons/Assets/Scripts/UIManager.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     int score = 0;
+    int objetivo = 20;
     [SerializeField] TextMeshProUGUI scoreText;
 
     public void AddScore()
     {
         score++;
         scoreText.text = score.ToString();
+    }
+
+    public void Update()
+    {
+        if(score >= objetivo)
+        {
+            SceneManager.LoadScene("Final");
+        }
     }
 }
