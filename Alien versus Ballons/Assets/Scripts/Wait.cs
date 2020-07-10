482025c1487af4a7aa4c5ecf04b4e4e46cfe4bdb
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2020-07-09 23:50:03 -0400

    Done

---
+++ Alien versus Ballons/Assets/Scripts/Wait.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    public float wait_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Waiting());
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene("Titulo");
+    }
+}
